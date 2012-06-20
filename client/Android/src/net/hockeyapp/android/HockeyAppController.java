485cb047fcf19a777191fdec7fec761f15175550
Alex Birkett <alex@birkett.co.uk>
2012-06-20 13:42:19 +0200

    CheckUpdateTaskNoGui is similar to CheckUpdateTask except that it calls back via an observer if an update is available rather than displaying the dialog itself. The method to create the dialog is now in a static method in UpdateDialogBuilder
    HockeyAppController uses CheckUpdateTaskNoGui and a static variable to store the update details because the the onRetainNonConfigurationInstance() method is deprecated and not available if the BaseControllerActivity extends FragmentActivity.
    
    CheckUpdateTask should be refactored to use CheckUpdateTaskNoGui and UpdateDialogBuilder in order to remove the copy paste code

--- client/Android/src/net/hockeyapp/android/HockeyAppController.java
+++ client/Android/src/net/hockeyapp/android/HockeyAppController.java
@@ -4,17 +4,28 @@ import android.app.Activity;
 import android.os.Bundle;
 import no.birkettconsulting.controllers.Controller;
 
-public class HockeyAppController extends Controller {
+public class HockeyAppController extends Controller implements CheckUpdateTaskNoGui.UpdateTaskObserver {
 	
-	private CheckUpdateTask checkUpdateTask;
+	private CheckUpdateTaskNoGui checkUpdateTask;
+	private String downloadUrl;
+	private String appId;
+	private boolean paused;
 
-	private String mDownloadUrl;
-	private String mAppId;
-
+	/**
+	 * Using a static instance variable is not ideal! It was done because the
+	 * onRetainNonConfigurationInstance() method is deprecated and not available
+	 * if the BaseControllerActivity extends FragmentActivity. It would be
+	 * possible to use a Fragment and retain the instance across Activity
+	 * recreation. Alternatively, a combination of a controller and a service
+	 * could be used instead but the service would need to be declared in the
+	 * app's AndroidManifest which is additional work for the developer.
+	 */
+	private static UpdateDetails updateDetails;
+	
 	public HockeyAppController(Activity activity, String downloadUrl, String appId) {
 		super(activity);
-		mAppId = appId;
-		mDownloadUrl = downloadUrl;
+		this.appId = appId;
+		this.downloadUrl = downloadUrl;
 	}
 
 	@Override
@@ -23,35 +34,49 @@ public class HockeyAppController extends Controller {
 	}
 	
 	private void checkForUpdates() {
-		checkUpdateTask = (CheckUpdateTask) getActivity()
-				.getLastNonConfigurationInstance();
-		if (checkUpdateTask != null) {
-			checkUpdateTask.attach(getActivity());
-		} else {
-			checkUpdateTask = new CheckUpdateTask(getActivity(),
-					mDownloadUrl, mAppId);
-			checkUpdateTask.execute();
+		if (HockeyAppController.updateDetails == null) {
+			checkUpdateTask = new CheckUpdateTaskNoGui(getActivity(), downloadUrl, appId, this);
+			checkUpdateTask.execute();			
 		}
 	}
 
+	private void showUpdateAvailableDialogIfUpdateDetailsSet() {
+		if (HockeyAppController.updateDetails != null) {
+			UpdateDialogBuiler.showDialog(getActivity(),
+					HockeyAppController.updateDetails);
+			HockeyAppController.updateDetails = null;
+		}
+		
+	}
+	
 	@Override
-	public Object onRetainNonConfigurationInstance() {
-		checkUpdateTask.detach();
-		return checkUpdateTask;
+	public void onPause() {
+		paused = true;
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
+		paused = false;
 		checkForCrashes();
+		showUpdateAvailableDialogIfUpdateDetailsSet();
 	}
 
 	private void checkForCrashes() {
-		CrashManager.register(mContext,mDownloadUrl, mAppId);
+		CrashManager.register(mContext,downloadUrl, appId);
 	}
 
 	private Activity getActivity() {
 		return (Activity) mContext;
+	}
+
+	@Override
+	public void onUpdateAvailable(UpdateDetails details) {
+		if (paused) {
+			HockeyAppController.updateDetails = details;
+		} else {
+			UpdateDialogBuiler.showDialog(getActivity(), details);
+		}
 	}
 
 }
